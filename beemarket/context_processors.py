71ea4204e3e60e054a1b7698815f3bf327dce096
Jose <backend2.muchaweb@gmail.com>
2015-07-18 17:57:58 -0500

    wishlist count

--- beemarket/context_processors.py
+++ beemarket/context_processors.py
@@ -9,7 +9,7 @@ def wishlists(request):
     if getattr(request, 'user', None) and request.user.is_authenticated():
         wish_lists = Wishlist.objects.filter(
             owner=request.user)
-        ctx['wish_lists'] = wish_lists
+        ctx['wish_list'] = wish_lists[0]
     return ctx
 
 #def wishlines(request):
